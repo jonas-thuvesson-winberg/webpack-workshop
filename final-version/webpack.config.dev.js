d06fa39a190d9dea092863d30777cf39644cbddf
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2019-02-05 13:07:43 +0100

    updated styles and changes how styles are handled in dev (not inline)

--- final-version/webpack.config.dev.js
+++ final-version/webpack.config.dev.js
@@ -3,31 +3,37 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.common');
 const webpack = require('webpack');
 const chokidar = require('chokidar');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'development',
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].[hash].css'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.scss$/,
-        exclude: [path.resolve(__dirname, 'node_modules')],
         include: [path.resolve(__dirname, 'src')],
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       }
     ]
   },
   devtool: 'source-map',
   devServer: {
     before: function(_, server) {
-      chokidar.watch(['./src/*.html']).on('all', function() {
+      chokidar.watch(['./src/*.html', './src/*.scss']).on('all', function() {
         server.sockWrite(server.sockets, 'content-changed');
       });
     },
     hot: true,
     contentBase: path.join(__dirname, 'dist'),
     watchContentBase: true,
-    // compress: true,
     port: 9000
   }
 });
